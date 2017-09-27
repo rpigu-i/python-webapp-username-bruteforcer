6e7b4e7bf59cd3235267f757cbdde2174a485691
Andy Dennis <8628078+patamechanix@users.noreply.github.com>
2017-09-27 11:50:37 -0400

    Added skeleton files for username sniffer tool

---
+++ README.md
@@ -0,0 +1,67 @@
+
+# Python WebApp Username Sniffer
+
+
+The following tool can be used to sniff for valid usernames
+where a web application gives some indication if a user is valid
+regardless if the password was wrong.
+
+## Configuration
+
+The script uses a YAML script for input paramters. This should be called user.yaml
+and be located in the same folder as the script
+
+Note: This will be made an input param in future releases. 
+
+The input params should be:
+
+* url - the target URL
+* user_field - The name of the user input field the API accepts
+* data - a list of other parameters that may be required by the API/App
+* response - used for recording the response field parameters that indiciate a valid user
+* response_key - nested under response and used for the key to check for a valid/invalid message
+* valid_response - nested under response, the value returned that confirm the a successful guess of a username.
+* users - a list of users
+
+The following YAML demonstrates the above:
+
+```
+url: "http://some.website/api/v1/login"
+user_field: "userName"
+
+data: null
+
+response:
+    response_key: message
+    valid_response: "Username correct but password not supplied"
+
+users:
+- bsmith@blah.com
+- jbloggs@blah.com
+
+```
+
+
+## Operation
+
+Run the script with:
+
+```
+python user_snuffer.py 
+
+```
+
+
+Once complete two YAML output files will be generated.
+
+valid_users.yaml - a list of valid usernames
+
+invalid_users - a list of usernames where no match was found
+
+
+
+
+
+
+
+
---
+++ user_sniffer.py
@@ -0,0 +1,111 @@
+import requests
+import json
+import yaml
+
+
+class UserSniffer():
+    """"
+    Class that takes input from
+    a YAML file and then attempts to
+    ascertain if a user account is
+    valid on target application
+    """
+
+    valid_users = {'users':[]}
+    invalid_users = {'users':[]}
+    yamldump = {}
+    user_field = ""
+    data = {}
+    url = ""
+    response_key = ""
+    valid_response = ""
+
+    def __init__(self):
+        """
+        Kick off the application
+        """
+        self.load_yaml()
+        self.load_params()
+        self.dump_yaml_output()
+
+
+    def load_yaml(self):
+        """"
+        Load params from
+        a YAML document
+        """
+        opendoc = open("users.yaml", "r")
+        self.yamldump = yaml.load_all(opendoc)
+
+
+    def load_params(self):
+        """
+        Load parameters
+        from yamldump
+        """ 
+        
+        for key in self.yamldump:
+        
+            self.data = {}
+            self.user_field = key['user_field']
+            self.response_key = key['response']['response_key']
+            self.valid_response = key['response']['valid_response']
+            self.url = key['url']
+            self.data[self.user_field] = ""
+        
+            if key['data'] and key['data'] != None:
+                data_vals = key['data']
+                for k in data_vals:
+                    self.data[k] = data_vals[k]
+            
+            if key['users']: 
+                users_list = key['users'] 
+                self.check_for_valid_users(users_list) 
+ 
+
+
+    def check_for_valid_users(self, users_list):
+        """
+        Using input parameters
+        test target url/api
+        to see if user exists
+        """         
+        
+        
+        for u in users_list:
+        
+            self.data[self.user_field] = u
+            r = requests.post(self.url, data=self.data) 
+        
+            dictdata = json.loads(r.text)
+        
+            if dictdata[self.response_key] == self.valid_response:
+        
+                print "Status code is: "+str(r.status_code) 
+                print "Response message is: "+str(r.reason)
+                print "Valid username: "+u 
+                self.valid_users['users'].append(u)
+            else:
+                self.invalid_users['users'].append(u)
+         
+        
+
+    def dump_yaml_output(self):
+        """
+        Dump a lsit of valid and
+        invalid users to two separate
+        docs
+        """
+        valid_users_doc = file('valid_users.yaml', 'w')
+        yaml.dump(self.valid_users, valid_users_doc, default_flow_style=False)
+        
+        invalid_users_doc = file('invalid_users.yaml', 'w')
+        yaml.dump(self.invalid_users, invalid_users_doc, default_flow_style=False)
+
+
+
+
+
+if __name__ == "__main__":
+    user_sniffer = UserSniffer()
+
